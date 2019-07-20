a0df909b08f15c70c6a9294977000323d5f32ad2
DevSrSouza <devsrsouza@gmail.com>
2019-07-20 17:30:05 -0300

    plugin instance in companion

--- src/main/kotlin/com/yourserver/yourplugin/YourPlugin.kt
+++ src/main/kotlin/com/yourserver/yourplugin/YourPlugin.kt
@@ -4,6 +4,15 @@ import org.bukkit.plugin.java.JavaPlugin
 
 class YourPlugin : JavaPlugin() {
 
+    companion object {
+        @JvmStatic lateinit var INSTANCE: YourPlugin
+            private set
+    }
+
+    override fun onLoad() {
+        INSTANCE = this
+    }
+
     override fun onEnable() {
         logger.info("My plugin with Kotlin is enabled")
     }
