5f91d59cf9243fbd6f4ea7b506347405e0ef03ad
Goh Wei Wen <goweiwen@gmail.com>
2020-04-18 17:31:18 +0800

    Refactor EnderDragon to not use Vector

--- src/main/kotlin/me/weiwen/blanktopia/EnderDragon.kt
+++ src/main/kotlin/me/weiwen/blanktopia/EnderDragon.kt
@@ -11,7 +11,6 @@ import org.bukkit.event.entity.EntityDeathEvent
 import org.bukkit.event.player.PlayerPortalEvent
 import org.bukkit.event.player.PlayerTeleportEvent
 import org.bukkit.inventory.ItemStack
-import org.bukkit.util.Vector
 
 class EnderDragon(val plugin: Blanktopia, val storage: Storage) : Module, Listener {
     override fun enable() {
@@ -34,10 +33,10 @@ class EnderDragon(val plugin: Blanktopia, val storage: Storage) : Module, Listen
                 if (enderDragonBattle.hasBeenPreviouslyKilled()
                     && enderDragonBattle.enderDragon == null) {
                     val loc = enderDragonBattle.endPortalLocation
-                    world.spawnEntity(loc.clone().add(Vector(3.5, 1.0, 0.5)), EntityType.ENDER_CRYSTAL)
-                    world.spawnEntity(loc.clone().add(Vector(-2.5, 1.0, 0.5)), EntityType.ENDER_CRYSTAL)
-                    world.spawnEntity(loc.clone().add(Vector(0.5, 1.0, 3.5)), EntityType.ENDER_CRYSTAL)
-                    world.spawnEntity(loc.clone().add(Vector(0.5, 1.0, -2.5)), EntityType.ENDER_CRYSTAL)
+                    world.spawnEntity(loc.clone().add(3.5, 1.0, 0.5), EntityType.ENDER_CRYSTAL)
+                    world.spawnEntity(loc.clone().add(-2.5, 1.0, 0.5), EntityType.ENDER_CRYSTAL)
+                    world.spawnEntity(loc.clone().add(0.5, 1.0, 3.5), EntityType.ENDER_CRYSTAL)
+                    world.spawnEntity(loc.clone().add(0.5, 1.0, -2.5), EntityType.ENDER_CRYSTAL)
                     enderDragonBattle.initiateRespawn()
                 }
             }
@@ -54,7 +53,7 @@ class EnderDragon(val plugin: Blanktopia, val storage: Storage) : Module, Listen
         Bukkit.getScheduler().runTaskLater(plugin, runTaskLater@{
             player?.inventory?.addItem(ItemStack(Material.ELYTRA))
             player?.giveExp(11500, true)
-            val eggLocation = enderDragonBattle.endPortalLocation.add(Vector(0, 4, 0))
+            val eggLocation = enderDragonBattle.endPortalLocation.add(0.0, 4.0, 0.0)
             val egg = world.getBlockAt(eggLocation)
             if (egg.type == Material.AIR) egg.type = Material.DRAGON_EGG
         } as () -> Unit, 200)
