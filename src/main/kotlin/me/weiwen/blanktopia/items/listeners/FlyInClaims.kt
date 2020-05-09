00ad7645e6126bddac46c83aa50617fdb2d2e105
Goh Wei Wen <goweiwen@gmail.com>
2020-05-10 05:00:55 +0800

    Disable Hummingbird in Creative/Spectator

--- src/main/kotlin/me/weiwen/blanktopia/items/listeners/FlyInClaims.kt
+++ src/main/kotlin/me/weiwen/blanktopia/items/listeners/FlyInClaims.kt
@@ -2,6 +2,7 @@ package me.weiwen.blanktopia.items.listeners
 
 import me.weiwen.blanktopia.Blanktopia
 import me.weiwen.blanktopia.isInTrustedClaim
+import org.bukkit.GameMode
 import org.bukkit.Location
 import org.bukkit.NamespacedKey
 import org.bukkit.entity.Player
@@ -24,7 +25,8 @@ class FlyInClaims(val plugin: Blanktopia) : Listener {
 
     private fun checkCanFly(player: Player, to: Location?, from: Location) {
         if (to == null) return
-        if (to.blockX == from.blockX && to.blockY == from.blockY && to.blockZ == from.blockZ) return
+        if (to.blockX == from.blockX && to.blockZ == from.blockZ) return
+        if (player.gameMode == GameMode.CREATIVE || player.gameMode == GameMode.SPECTATOR) return
         if (player.persistentDataContainer.get(NamespacedKey(plugin, "fly-in-claims"), PersistentDataType.BYTE) == 1.toByte()) {
             player.allowFlight = isInTrustedClaim(player, to)
         }
