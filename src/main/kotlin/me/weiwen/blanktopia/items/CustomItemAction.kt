fea3fe43861017cfd9295df4506cd2b2a20a896b
Goh Wei Wen <goweiwen@gmail.com>
2020-05-23 22:26:11 +0800

    Add support for carpets to Paint Brush

--- src/main/kotlin/me/weiwen/blanktopia/items/CustomItemAction.kt
+++ src/main/kotlin/me/weiwen/blanktopia/items/CustomItemAction.kt
@@ -217,6 +217,7 @@ val BLOCK_COLOUR_MAP: Map<Material, DyeColor> = mapOf(
     Pair(Material.WHITE_CONCRETE, DyeColor.WHITE),
     Pair(Material.WHITE_CONCRETE_POWDER, DyeColor.WHITE),
     Pair(Material.WHITE_BED, DyeColor.WHITE),
+    Pair(Material.WHITE_CARPET, DyeColor.WHITE),
 
     Pair(Material.ORANGE_WOOL, DyeColor.ORANGE),
     Pair(Material.ORANGE_STAINED_GLASS, DyeColor.ORANGE),
@@ -226,6 +227,7 @@ val BLOCK_COLOUR_MAP: Map<Material, DyeColor> = mapOf(
     Pair(Material.ORANGE_CONCRETE, DyeColor.ORANGE),
     Pair(Material.ORANGE_CONCRETE_POWDER, DyeColor.ORANGE),
     Pair(Material.ORANGE_BED, DyeColor.ORANGE),
+    Pair(Material.ORANGE_CARPET, DyeColor.ORANGE),
 
     Pair(Material.MAGENTA_WOOL, DyeColor.MAGENTA),
     Pair(Material.MAGENTA_STAINED_GLASS, DyeColor.MAGENTA),
@@ -235,6 +237,7 @@ val BLOCK_COLOUR_MAP: Map<Material, DyeColor> = mapOf(
     Pair(Material.MAGENTA_CONCRETE, DyeColor.MAGENTA),
     Pair(Material.MAGENTA_CONCRETE_POWDER, DyeColor.MAGENTA),
     Pair(Material.MAGENTA_BED, DyeColor.MAGENTA),
+    Pair(Material.MAGENTA_CARPET, DyeColor.MAGENTA),
 
     Pair(Material.LIGHT_BLUE_WOOL, DyeColor.LIGHT_BLUE),
     Pair(Material.LIGHT_BLUE_STAINED_GLASS, DyeColor.LIGHT_BLUE),
@@ -244,6 +247,7 @@ val BLOCK_COLOUR_MAP: Map<Material, DyeColor> = mapOf(
     Pair(Material.LIGHT_BLUE_CONCRETE, DyeColor.LIGHT_BLUE),
     Pair(Material.LIGHT_BLUE_CONCRETE_POWDER, DyeColor.LIGHT_BLUE),
     Pair(Material.LIGHT_BLUE_BED, DyeColor.LIGHT_BLUE),
+    Pair(Material.LIGHT_BLUE_CARPET, DyeColor.LIGHT_BLUE),
 
     Pair(Material.YELLOW_WOOL, DyeColor.YELLOW),
     Pair(Material.YELLOW_STAINED_GLASS, DyeColor.YELLOW),
@@ -253,6 +257,7 @@ val BLOCK_COLOUR_MAP: Map<Material, DyeColor> = mapOf(
     Pair(Material.YELLOW_CONCRETE, DyeColor.YELLOW),
     Pair(Material.YELLOW_CONCRETE_POWDER, DyeColor.YELLOW),
     Pair(Material.YELLOW_BED, DyeColor.YELLOW),
+    Pair(Material.YELLOW_CARPET, DyeColor.YELLOW),
 
     Pair(Material.LIME_WOOL, DyeColor.LIME),
     Pair(Material.LIME_STAINED_GLASS, DyeColor.LIME),
@@ -262,6 +267,7 @@ val BLOCK_COLOUR_MAP: Map<Material, DyeColor> = mapOf(
     Pair(Material.LIME_CONCRETE, DyeColor.LIME),
     Pair(Material.LIME_CONCRETE_POWDER, DyeColor.LIME),
     Pair(Material.LIME_BED, DyeColor.LIME),
+    Pair(Material.LIME_CARPET, DyeColor.LIME),
 
     Pair(Material.PINK_WOOL, DyeColor.PINK),
     Pair(Material.PINK_STAINED_GLASS, DyeColor.PINK),
@@ -271,6 +277,7 @@ val BLOCK_COLOUR_MAP: Map<Material, DyeColor> = mapOf(
     Pair(Material.PINK_CONCRETE, DyeColor.PINK),
     Pair(Material.PINK_CONCRETE_POWDER, DyeColor.PINK),
     Pair(Material.PINK_BED, DyeColor.PINK),
+    Pair(Material.PINK_CARPET, DyeColor.PINK),
 
     Pair(Material.GRAY_WOOL, DyeColor.GRAY),
     Pair(Material.GRAY_STAINED_GLASS, DyeColor.GRAY),
@@ -280,6 +287,7 @@ val BLOCK_COLOUR_MAP: Map<Material, DyeColor> = mapOf(
     Pair(Material.GRAY_CONCRETE, DyeColor.GRAY),
     Pair(Material.GRAY_CONCRETE_POWDER, DyeColor.GRAY),
     Pair(Material.GRAY_BED, DyeColor.GRAY),
+    Pair(Material.GRAY_CARPET, DyeColor.GRAY),
 
     Pair(Material.LIGHT_GRAY_WOOL, DyeColor.LIGHT_GRAY),
     Pair(Material.LIGHT_GRAY_STAINED_GLASS, DyeColor.LIGHT_GRAY),
@@ -289,6 +297,7 @@ val BLOCK_COLOUR_MAP: Map<Material, DyeColor> = mapOf(
     Pair(Material.LIGHT_GRAY_CONCRETE, DyeColor.LIGHT_GRAY),
     Pair(Material.LIGHT_GRAY_CONCRETE_POWDER, DyeColor.LIGHT_GRAY),
     Pair(Material.LIGHT_GRAY_BED, DyeColor.LIGHT_GRAY),
+    Pair(Material.LIGHT_GRAY_CARPET, DyeColor.LIGHT_GRAY),
 
     Pair(Material.CYAN_WOOL, DyeColor.CYAN),
     Pair(Material.CYAN_STAINED_GLASS, DyeColor.CYAN),
@@ -298,6 +307,7 @@ val BLOCK_COLOUR_MAP: Map<Material, DyeColor> = mapOf(
     Pair(Material.CYAN_CONCRETE, DyeColor.CYAN),
     Pair(Material.CYAN_CONCRETE_POWDER, DyeColor.CYAN),
     Pair(Material.CYAN_BED, DyeColor.CYAN),
+    Pair(Material.CYAN_CARPET, DyeColor.CYAN),
 
     Pair(Material.PURPLE_WOOL, DyeColor.PURPLE),
     Pair(Material.PURPLE_STAINED_GLASS, DyeColor.PURPLE),
@@ -307,6 +317,7 @@ val BLOCK_COLOUR_MAP: Map<Material, DyeColor> = mapOf(
     Pair(Material.PURPLE_CONCRETE, DyeColor.PURPLE),
     Pair(Material.PURPLE_CONCRETE_POWDER, DyeColor.PURPLE),
     Pair(Material.PURPLE_BED, DyeColor.PURPLE),
+    Pair(Material.PURPLE_CARPET, DyeColor.PURPLE),
 
     Pair(Material.BLUE_WOOL, DyeColor.BLUE),
     Pair(Material.BLUE_STAINED_GLASS, DyeColor.BLUE),
@@ -316,6 +327,7 @@ val BLOCK_COLOUR_MAP: Map<Material, DyeColor> = mapOf(
     Pair(Material.BLUE_CONCRETE, DyeColor.BLUE),
     Pair(Material.BLUE_CONCRETE_POWDER, DyeColor.BLUE),
     Pair(Material.BLUE_BED, DyeColor.BLUE),
+    Pair(Material.BLUE_CARPET, DyeColor.BLUE),
 
     Pair(Material.BROWN_WOOL, DyeColor.BROWN),
     Pair(Material.BROWN_STAINED_GLASS, DyeColor.BROWN),
@@ -325,6 +337,7 @@ val BLOCK_COLOUR_MAP: Map<Material, DyeColor> = mapOf(
     Pair(Material.BROWN_CONCRETE, DyeColor.BROWN),
     Pair(Material.BROWN_CONCRETE_POWDER, DyeColor.BROWN),
     Pair(Material.BROWN_BED, DyeColor.BROWN),
+    Pair(Material.BROWN_CARPET, DyeColor.BROWN),
 
     Pair(Material.GREEN_WOOL, DyeColor.GREEN),
     Pair(Material.GREEN_STAINED_GLASS, DyeColor.GREEN),
@@ -334,6 +347,7 @@ val BLOCK_COLOUR_MAP: Map<Material, DyeColor> = mapOf(
     Pair(Material.GREEN_CONCRETE, DyeColor.GREEN),
     Pair(Material.GREEN_CONCRETE_POWDER, DyeColor.GREEN),
     Pair(Material.GREEN_BED, DyeColor.GREEN),
+    Pair(Material.GREEN_CARPET, DyeColor.GREEN),
 
     Pair(Material.RED_WOOL, DyeColor.RED),
     Pair(Material.RED_STAINED_GLASS, DyeColor.RED),
@@ -343,6 +357,7 @@ val BLOCK_COLOUR_MAP: Map<Material, DyeColor> = mapOf(
     Pair(Material.RED_CONCRETE, DyeColor.RED),
     Pair(Material.RED_CONCRETE_POWDER, DyeColor.RED),
     Pair(Material.RED_BED, DyeColor.RED),
+    Pair(Material.RED_CARPET, DyeColor.RED),
 
     Pair(Material.BLACK_WOOL, DyeColor.BLACK),
     Pair(Material.BLACK_STAINED_GLASS, DyeColor.BLACK),
@@ -351,7 +366,8 @@ val BLOCK_COLOUR_MAP: Map<Material, DyeColor> = mapOf(
     Pair(Material.BLACK_GLAZED_TERRACOTTA, DyeColor.BLACK),
     Pair(Material.BLACK_CONCRETE, DyeColor.BLACK),
     Pair(Material.BLACK_CONCRETE_POWDER, DyeColor.BLACK),
-    Pair(Material.BLACK_BED, DyeColor.BLACK)
+    Pair(Material.BLACK_BED, DyeColor.BLACK),
+    Pair(Material.BLACK_CARPET, DyeColor.BLACK)
 )
 fun paintBrushPick(
     player: Player,
@@ -530,6 +546,25 @@ val CONCRETE_POWDER_MAP: Map<DyeColor, Material> = mapOf(
     Pair(DyeColor.RED, Material.RED_CONCRETE_POWDER),
     Pair(DyeColor.BLACK, Material.BLACK_CONCRETE_POWDER)
 )
+val CARPET: List<Material> = listOf(Material.WHITE_CARPET, Material.ORANGE_CARPET, Material.MAGENTA_CARPET, Material.LIGHT_BLUE_CARPET, Material.YELLOW_CARPET, Material.LIME_CARPET, Material.PINK_CARPET, Material.GRAY_CARPET, Material.LIGHT_GRAY_CARPET, Material.CYAN_CARPET, Material.PURPLE_CARPET, Material.BLUE_CARPET, Material.BROWN_CARPET, Material.GREEN_CARPET, Material.RED_CARPET, Material.BLACK_CARPET)
+val CARPET_MAP: Map<DyeColor, Material> = mapOf(
+    Pair(DyeColor.WHITE, Material.WHITE_CARPET),
+    Pair(DyeColor.ORANGE, Material.ORANGE_CARPET),
+    Pair(DyeColor.MAGENTA, Material.MAGENTA_CARPET),
+    Pair(DyeColor.LIGHT_BLUE, Material.LIGHT_BLUE_CARPET),
+    Pair(DyeColor.YELLOW, Material.YELLOW_CARPET),
+    Pair(DyeColor.LIME, Material.LIME_CARPET),
+    Pair(DyeColor.PINK, Material.PINK_CARPET),
+    Pair(DyeColor.GRAY, Material.GRAY_CARPET),
+    Pair(DyeColor.LIGHT_GRAY, Material.LIGHT_GRAY_CARPET),
+    Pair(DyeColor.CYAN, Material.CYAN_CARPET),
+    Pair(DyeColor.PURPLE, Material.PURPLE_CARPET),
+    Pair(DyeColor.BLUE, Material.BLUE_CARPET),
+    Pair(DyeColor.BROWN, Material.BROWN_CARPET),
+    Pair(DyeColor.GREEN, Material.GREEN_CARPET),
+    Pair(DyeColor.RED, Material.RED_CARPET),
+    Pair(DyeColor.BLACK, Material.BLACK_CARPET)
+)
 
 fun paintBrushPaint(
     player: Player,
@@ -573,6 +608,8 @@ fun paintBrushPaint(
         block.type = CONCRETE_MAP[colour]!!
     } else if (block.type in CONCRETE_POWDER) {
         block.type = CONCRETE_POWDER_MAP[colour]!!
+    } else if (block.type in CARPET) {
+        block.type = CARPET_MAP[colour]!!
     } else {
         return
     }
