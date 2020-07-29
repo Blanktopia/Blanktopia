47aa2fe0579b0133b93417c5deb0443e9c5d2283
Goh Wei Wen <goweiwen@gmail.com>
2020-07-30 00:54:23 +0800

    Fix ActionBarAction

--- BlanktopiaItems/src/main/kotlin/me/weiwen/blanktopia/actions/ActionBarAction.kt
+++ BlanktopiaItems/src/main/kotlin/me/weiwen/blanktopia/actions/ActionBarAction.kt
@@ -6,6 +6,6 @@ import org.bukkit.entity.Player
 class ActionBarAction(private val message: String) : Action {
     override fun run(player: Player) {
         val formatted = TextComponent(*TextComponent.fromLegacyText(message))
-        player.spigot().sendMessage(formatted)
+        player.sendActionBar(formatted)
     }
 }
