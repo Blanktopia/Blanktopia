c2d590bc690ad5d81506888c430caa82c838eab4
Goh Wei Wen <goweiwen@gmail.com>
2020-08-09 23:04:09 +0800

    Refactor SudoCommandAction

--- BlanktopiaItems/src/main/kotlin/me/weiwen/blanktopia/actions/SudoCommandAction.kt
+++ BlanktopiaItems/src/main/kotlin/me/weiwen/blanktopia/actions/SudoCommandAction.kt
@@ -2,22 +2,18 @@ package me.weiwen.blanktopia.actions
 
 import me.weiwen.blanktopia.BlanktopiaItems
 import org.bukkit.entity.Player
+import org.bukkit.permissions.Permission
 
 class SudoCommandAction(private val command: String) : Action {
     override fun run(player: Player) {
         val formatted = command.replace("%p", player.name)
-        val permission = player.addAttachment(BlanktopiaItems.INSTANCE, "*", true)
-        val isOp = player.isOp
-        if (!isOp) {
-            player.isOp = true
-        }
-        try {
-            player.performCommand(formatted)
-        } finally {
-            if (!isOp) {
-                player.isOp = false
-            }
-            permission.remove()
-        }
+        player.asOp(true).performCommand(formatted)
     }
+}
+
+private fun Player.asOp(allPerms: Boolean = false) = OppedPlayer(this, allPerms)
+private class OppedPlayer(val sub: Player, val allPerms: Boolean = true) : Player by sub {
+    override fun isOp() = true
+    override fun hasPermission(name: String): Boolean = allPerms || sub.hasPermission(name)
+    override fun hasPermission(perm: Permission): Boolean = allPerms || sub.hasPermission(perm)
 }
