b0c582b9f235bc1cea04cb9357e63fcdac7f656f
Goh Wei Wen <goweiwen@gmail.com>
2020-10-16 07:30:25 +0800

    Use proper block drop manipulation for Smelt enchantment

--- Blanktopia/src/main/kotlin/me/weiwen/blanktopia/enchants/enchantments/Smelt.kt
+++ Blanktopia/src/main/kotlin/me/weiwen/blanktopia/enchants/enchantments/Smelt.kt
@@ -11,6 +11,7 @@ import org.bukkit.entity.EntityType
 import org.bukkit.entity.ExperienceOrb
 import org.bukkit.entity.Item
 import org.bukkit.event.EventHandler
+import org.bukkit.event.EventPriority
 import org.bukkit.event.Listener
 import org.bukkit.event.block.BlockDropItemEvent
 import org.bukkit.inventory.ItemStack
@@ -33,10 +34,12 @@ val SMELT = CustomEnchantment(
     Smelt
 )
 
+data class SmeltResult(val material: Material, val experience: Double)
+
 object Smelt : Listener {
     init {}
 
-    @EventHandler
+    @EventHandler(priority = EventPriority.LOW)
     fun onBlockDropItem(event: BlockDropItemEvent) {
         val player = event.player
         val tool = player.inventory.itemInMainHand
@@ -45,17 +48,15 @@ object Smelt : Listener {
         var smelted = false
         var experience = 0.0
         val items = event.items
-        val removedItems = mutableSetOf<Item>()
         for (item in items) {
-            val smeltedItem = getSmeltedDrops(item.itemStack)
-            if (smeltedItem != null) {
-                removedItems.add(item)
-                smelted = true
-                block.world.dropItemNaturally(block.location, smeltedItem.first)
-                experience += smeltedItem.second
-            }
+            val smeltedItem = getSmeltedDrops(item.itemStack) ?: continue
+            smelted = true
+            experience += smeltedItem.experience
+
+            val itemStack = item.itemStack
+            itemStack.type = smeltedItem.material
+            item.itemStack = itemStack
         }
-        items.removeAll(removedItems)
         if (smelted) {
             block.spawnParticleAt(Particle.FLAME, 1, 0.01)
             block.playSoundAt(Sound.BLOCK_FIRE_AMBIENT, SoundCategory.BLOCKS, 0.5f, 0.8f)
@@ -70,84 +71,83 @@ object Smelt : Listener {
         }
     }
 
-    private fun getSmeltedDrops(item: ItemStack): Pair<ItemStack, Double>? {
-        val amount = item.amount
+    private fun getSmeltedDrops(item: ItemStack): SmeltResult? {
         return when (item.type) {
-            Material.PORKCHOP -> Pair(ItemStack(Material.COOKED_PORKCHOP, amount), 0.0)
-            Material.BEEF -> Pair(ItemStack(Material.COOKED_BEEF, amount), 0.0)
-            Material.CHICKEN -> Pair(ItemStack(Material.COOKED_CHICKEN, amount), 0.0)
-            Material.COD -> Pair(ItemStack(Material.COOKED_COD, amount), 0.0)
-            Material.SALMON -> Pair(ItemStack(Material.COOKED_SALMON, amount), 0.0)
-            Material.POTATO -> Pair(ItemStack(Material.BAKED_POTATO, amount), 0.0)
-            Material.MUTTON -> Pair(ItemStack(Material.COOKED_MUTTON, amount), 0.0)
-            Material.RABBIT -> Pair(ItemStack(Material.COOKED_RABBIT, amount), 0.0)
-            Material.KELP -> Pair(ItemStack(Material.DRIED_KELP, amount), 0.0)
-            Material.KELP_PLANT -> Pair(ItemStack(Material.DRIED_KELP, amount), 0.0)
-            Material.IRON_ORE -> Pair(ItemStack(Material.IRON_INGOT, amount), 0.035)
-            Material.GOLD_ORE -> Pair(ItemStack(Material.GOLD_INGOT, amount), 0.5)
-            Material.SAND -> Pair(ItemStack(Material.GLASS, amount), 0.0)
-            Material.COBBLESTONE -> Pair(ItemStack(Material.STONE, amount), 0.00)
-            Material.STONE -> Pair(ItemStack(Material.SMOOTH_STONE, amount), 0.0)
-            Material.SANDSTONE -> Pair(ItemStack(Material.SMOOTH_SANDSTONE, amount), 0.0)
-            Material.RED_SANDSTONE -> Pair(ItemStack(Material.SMOOTH_RED_SANDSTONE, amount), 0.0)
-            Material.QUARTZ_BLOCK -> Pair(ItemStack(Material.SMOOTH_QUARTZ, amount), 0.0)
-            Material.NETHERRACK -> Pair(ItemStack(Material.NETHER_BRICK, amount), 0.0)
-            Material.CLAY_BALL -> Pair(ItemStack(Material.BRICK, amount), 0.0)
-            Material.CLAY -> Pair(ItemStack(Material.TERRACOTTA, amount), 0.0)
-            Material.STONE_BRICKS -> Pair(ItemStack(Material.CRACKED_STONE_BRICKS, amount), 0.0)
-            Material.WHITE_TERRACOTTA -> Pair(ItemStack(Material.WHITE_GLAZED_TERRACOTTA, amount), 0.0)
-            Material.ORANGE_TERRACOTTA -> Pair(ItemStack(Material.ORANGE_GLAZED_TERRACOTTA, amount), 0.0)
-            Material.MAGENTA_TERRACOTTA -> Pair(ItemStack(Material.MAGENTA_GLAZED_TERRACOTTA, amount), 0.0)
-            Material.LIGHT_BLUE_TERRACOTTA -> Pair(ItemStack(Material.LIGHT_BLUE_GLAZED_TERRACOTTA, amount), 0.0)
-            Material.YELLOW_TERRACOTTA -> Pair(ItemStack(Material.YELLOW_GLAZED_TERRACOTTA, amount), 0.0)
-            Material.LIME_TERRACOTTA -> Pair(ItemStack(Material.LIME_GLAZED_TERRACOTTA, amount), 0.0)
-            Material.PINK_TERRACOTTA -> Pair(ItemStack(Material.PINK_GLAZED_TERRACOTTA, amount), 0.0)
-            Material.GRAY_TERRACOTTA -> Pair(ItemStack(Material.GRAY_GLAZED_TERRACOTTA, amount), 0.0)
-            Material.LIGHT_GRAY_TERRACOTTA -> Pair(ItemStack(Material.LIGHT_GRAY_GLAZED_TERRACOTTA, amount), 0.0)
-            Material.CYAN_TERRACOTTA -> Pair(ItemStack(Material.CYAN_GLAZED_TERRACOTTA, amount), 0.0)
-            Material.BLUE_TERRACOTTA -> Pair(ItemStack(Material.BLUE_GLAZED_TERRACOTTA, amount), 0.0)
-            Material.PURPLE_TERRACOTTA -> Pair(ItemStack(Material.PURPLE_GLAZED_TERRACOTTA, amount), 0.0)
-            Material.GREEN_TERRACOTTA -> Pair(ItemStack(Material.GREEN_GLAZED_TERRACOTTA, amount), 0.0)
-            Material.BROWN_TERRACOTTA -> Pair(ItemStack(Material.BROWN_GLAZED_TERRACOTTA, amount), 0.0)
-            Material.RED_TERRACOTTA -> Pair(ItemStack(Material.RED_GLAZED_TERRACOTTA, amount), 0.0)
-            Material.BLACK_TERRACOTTA -> Pair(ItemStack(Material.BLACK_GLAZED_TERRACOTTA, amount), 0.0)
-            Material.CACTUS -> Pair(ItemStack(Material.GREEN_DYE, amount), 0.0)
-            Material.ACACIA_LOG -> Pair(ItemStack(Material.CHARCOAL, amount), 0.0)
-            Material.BIRCH_LOG -> Pair(ItemStack(Material.CHARCOAL, amount), 0.0)
-            Material.DARK_OAK_LOG -> Pair(ItemStack(Material.CHARCOAL, amount), 0.0)
-            Material.JUNGLE_LOG -> Pair(ItemStack(Material.CHARCOAL, amount), 0.0)
-            Material.OAK_LOG -> Pair(ItemStack(Material.CHARCOAL, amount), 0.0)
-            Material.SPRUCE_LOG -> Pair(ItemStack(Material.CHARCOAL, amount), 0.0)
-            Material.ACACIA_WOOD -> Pair(ItemStack(Material.CHARCOAL, amount), 0.0)
-            Material.BIRCH_WOOD -> Pair(ItemStack(Material.CHARCOAL, amount), 0.0)
-            Material.DARK_OAK_WOOD -> Pair(ItemStack(Material.CHARCOAL, amount), 0.0)
-            Material.JUNGLE_WOOD -> Pair(ItemStack(Material.CHARCOAL, amount), 0.0)
-            Material.OAK_WOOD -> Pair(ItemStack(Material.CHARCOAL, amount), 0.0)
-            Material.SPRUCE_WOOD -> Pair(ItemStack(Material.CHARCOAL, amount), 0.0)
-            Material.STRIPPED_ACACIA_LOG -> Pair(ItemStack(Material.CHARCOAL, amount), 0.0)
-            Material.STRIPPED_BIRCH_LOG -> Pair(ItemStack(Material.CHARCOAL, amount), 0.0)
-            Material.STRIPPED_DARK_OAK_LOG -> Pair(ItemStack(Material.CHARCOAL, amount), 0.0)
-            Material.STRIPPED_JUNGLE_LOG -> Pair(ItemStack(Material.CHARCOAL, amount), 0.0)
-            Material.STRIPPED_OAK_LOG -> Pair(ItemStack(Material.CHARCOAL, amount), 0.0)
-            Material.STRIPPED_SPRUCE_LOG -> Pair(ItemStack(Material.CHARCOAL, amount), 0.0)
-            Material.STRIPPED_ACACIA_WOOD -> Pair(ItemStack(Material.CHARCOAL, amount), 0.0)
-            Material.STRIPPED_BIRCH_WOOD -> Pair(ItemStack(Material.CHARCOAL, amount), 0.0)
-            Material.STRIPPED_DARK_OAK_WOOD -> Pair(ItemStack(Material.CHARCOAL, amount), 0.0)
-            Material.STRIPPED_JUNGLE_WOOD -> Pair(ItemStack(Material.CHARCOAL, amount), 0.0)
-            Material.STRIPPED_OAK_WOOD -> Pair(ItemStack(Material.CHARCOAL, amount), 0.0)
-            Material.STRIPPED_SPRUCE_WOOD -> Pair(ItemStack(Material.CHARCOAL, amount), 0.0)
-            Material.CHORUS_FRUIT -> Pair(ItemStack(Material.POPPED_CHORUS_FRUIT, amount), 0.0)
-            Material.WET_SPONGE -> Pair(ItemStack(Material.SPONGE, amount), 0.0)
-            Material.SEA_PICKLE -> Pair(ItemStack(Material.LIME_DYE, amount), 0.0)
-            Material.ANCIENT_DEBRIS -> Pair(ItemStack(Material.NETHERITE_SCRAP, amount), 0.0)
-            Material.WARPED_HYPHAE -> Pair(ItemStack(Material.CHARCOAL, amount), 0.0)
-            Material.WARPED_STEM -> Pair(ItemStack(Material.CHARCOAL, amount), 0.0)
-            Material.STRIPPED_WARPED_HYPHAE -> Pair(ItemStack(Material.CHARCOAL, amount), 0.0)
-            Material.STRIPPED_WARPED_STEM -> Pair(ItemStack(Material.CHARCOAL, amount), 0.0)
-            Material.CRIMSON_HYPHAE -> Pair(ItemStack(Material.CHARCOAL, amount), 0.0)
-            Material.CRIMSON_STEM -> Pair(ItemStack(Material.CHARCOAL, amount), 0.0)
-            Material.STRIPPED_CRIMSON_HYPHAE -> Pair(ItemStack(Material.CHARCOAL, amount), 0.0)
-            Material.STRIPPED_CRIMSON_STEM -> Pair(ItemStack(Material.CHARCOAL, amount), 0.0)
+            Material.PORKCHOP -> SmeltResult(Material.COOKED_PORKCHOP, 0.0)
+            Material.BEEF -> SmeltResult(Material.COOKED_BEEF, 0.0)
+            Material.CHICKEN -> SmeltResult(Material.COOKED_CHICKEN, 0.0)
+            Material.COD -> SmeltResult(Material.COOKED_COD, 0.0)
+            Material.SALMON -> SmeltResult(Material.COOKED_SALMON, 0.0)
+            Material.POTATO -> SmeltResult(Material.BAKED_POTATO, 0.0)
+            Material.MUTTON -> SmeltResult(Material.COOKED_MUTTON, 0.0)
+            Material.RABBIT -> SmeltResult(Material.COOKED_RABBIT, 0.0)
+            Material.KELP -> SmeltResult(Material.DRIED_KELP, 0.0)
+            Material.KELP_PLANT -> SmeltResult(Material.DRIED_KELP, 0.0)
+            Material.IRON_ORE -> SmeltResult(Material.IRON_INGOT, 0.035)
+            Material.GOLD_ORE -> SmeltResult(Material.GOLD_INGOT, 0.5)
+            Material.SAND -> SmeltResult(Material.GLASS, 0.0)
+            Material.COBBLESTONE -> SmeltResult(Material.STONE, 0.00)
+            Material.STONE -> SmeltResult(Material.SMOOTH_STONE, 0.0)
+            Material.SANDSTONE -> SmeltResult(Material.SMOOTH_SANDSTONE, 0.0)
+            Material.RED_SANDSTONE -> SmeltResult(Material.SMOOTH_RED_SANDSTONE, 0.0)
+            Material.QUARTZ_BLOCK -> SmeltResult(Material.SMOOTH_QUARTZ, 0.0)
+            Material.NETHERRACK -> SmeltResult(Material.NETHER_BRICK, 0.0)
+            Material.CLAY_BALL -> SmeltResult(Material.BRICK, 0.0)
+            Material.CLAY -> SmeltResult(Material.TERRACOTTA, 0.0)
+            Material.STONE_BRICKS -> SmeltResult(Material.CRACKED_STONE_BRICKS, 0.0)
+            Material.WHITE_TERRACOTTA -> SmeltResult(Material.WHITE_GLAZED_TERRACOTTA, 0.0)
+            Material.ORANGE_TERRACOTTA -> SmeltResult(Material.ORANGE_GLAZED_TERRACOTTA, 0.0)
+            Material.MAGENTA_TERRACOTTA -> SmeltResult(Material.MAGENTA_GLAZED_TERRACOTTA, 0.0)
+            Material.LIGHT_BLUE_TERRACOTTA -> SmeltResult(Material.LIGHT_BLUE_GLAZED_TERRACOTTA, 0.0)
+            Material.YELLOW_TERRACOTTA -> SmeltResult(Material.YELLOW_GLAZED_TERRACOTTA, 0.0)
+            Material.LIME_TERRACOTTA -> SmeltResult(Material.LIME_GLAZED_TERRACOTTA, 0.0)
+            Material.PINK_TERRACOTTA -> SmeltResult(Material.PINK_GLAZED_TERRACOTTA, 0.0)
+            Material.GRAY_TERRACOTTA -> SmeltResult(Material.GRAY_GLAZED_TERRACOTTA, 0.0)
+            Material.LIGHT_GRAY_TERRACOTTA -> SmeltResult(Material.LIGHT_GRAY_GLAZED_TERRACOTTA, 0.0)
+            Material.CYAN_TERRACOTTA -> SmeltResult(Material.CYAN_GLAZED_TERRACOTTA, 0.0)
+            Material.BLUE_TERRACOTTA -> SmeltResult(Material.BLUE_GLAZED_TERRACOTTA, 0.0)
+            Material.PURPLE_TERRACOTTA -> SmeltResult(Material.PURPLE_GLAZED_TERRACOTTA, 0.0)
+            Material.GREEN_TERRACOTTA -> SmeltResult(Material.GREEN_GLAZED_TERRACOTTA, 0.0)
+            Material.BROWN_TERRACOTTA -> SmeltResult(Material.BROWN_GLAZED_TERRACOTTA, 0.0)
+            Material.RED_TERRACOTTA -> SmeltResult(Material.RED_GLAZED_TERRACOTTA, 0.0)
+            Material.BLACK_TERRACOTTA -> SmeltResult(Material.BLACK_GLAZED_TERRACOTTA, 0.0)
+            Material.CACTUS -> SmeltResult(Material.GREEN_DYE, 0.0)
+            Material.ACACIA_LOG -> SmeltResult(Material.CHARCOAL, 0.0)
+            Material.BIRCH_LOG -> SmeltResult(Material.CHARCOAL, 0.0)
+            Material.DARK_OAK_LOG -> SmeltResult(Material.CHARCOAL, 0.0)
+            Material.JUNGLE_LOG -> SmeltResult(Material.CHARCOAL, 0.0)
+            Material.OAK_LOG -> SmeltResult(Material.CHARCOAL, 0.0)
+            Material.SPRUCE_LOG -> SmeltResult(Material.CHARCOAL, 0.0)
+            Material.ACACIA_WOOD -> SmeltResult(Material.CHARCOAL, 0.0)
+            Material.BIRCH_WOOD -> SmeltResult(Material.CHARCOAL, 0.0)
+            Material.DARK_OAK_WOOD -> SmeltResult(Material.CHARCOAL, 0.0)
+            Material.JUNGLE_WOOD -> SmeltResult(Material.CHARCOAL, 0.0)
+            Material.OAK_WOOD -> SmeltResult(Material.CHARCOAL, 0.0)
+            Material.SPRUCE_WOOD -> SmeltResult(Material.CHARCOAL, 0.0)
+            Material.STRIPPED_ACACIA_LOG -> SmeltResult(Material.CHARCOAL, 0.0)
+            Material.STRIPPED_BIRCH_LOG -> SmeltResult(Material.CHARCOAL, 0.0)
+            Material.STRIPPED_DARK_OAK_LOG -> SmeltResult(Material.CHARCOAL, 0.0)
+            Material.STRIPPED_JUNGLE_LOG -> SmeltResult(Material.CHARCOAL, 0.0)
+            Material.STRIPPED_OAK_LOG -> SmeltResult(Material.CHARCOAL, 0.0)
+            Material.STRIPPED_SPRUCE_LOG -> SmeltResult(Material.CHARCOAL, 0.0)
+            Material.STRIPPED_ACACIA_WOOD -> SmeltResult(Material.CHARCOAL, 0.0)
+            Material.STRIPPED_BIRCH_WOOD -> SmeltResult(Material.CHARCOAL, 0.0)
+            Material.STRIPPED_DARK_OAK_WOOD -> SmeltResult(Material.CHARCOAL, 0.0)
+            Material.STRIPPED_JUNGLE_WOOD -> SmeltResult(Material.CHARCOAL, 0.0)
+            Material.STRIPPED_OAK_WOOD -> SmeltResult(Material.CHARCOAL, 0.0)
+            Material.STRIPPED_SPRUCE_WOOD -> SmeltResult(Material.CHARCOAL, 0.0)
+            Material.CHORUS_FRUIT -> SmeltResult(Material.POPPED_CHORUS_FRUIT, 0.0)
+            Material.WET_SPONGE -> SmeltResult(Material.SPONGE, 0.0)
+            Material.SEA_PICKLE -> SmeltResult(Material.LIME_DYE, 0.0)
+            Material.ANCIENT_DEBRIS -> SmeltResult(Material.NETHERITE_SCRAP, 0.0)
+            Material.WARPED_HYPHAE -> SmeltResult(Material.CHARCOAL, 0.0)
+            Material.WARPED_STEM -> SmeltResult(Material.CHARCOAL, 0.0)
+            Material.STRIPPED_WARPED_HYPHAE -> SmeltResult(Material.CHARCOAL, 0.0)
+            Material.STRIPPED_WARPED_STEM -> SmeltResult(Material.CHARCOAL, 0.0)
+            Material.CRIMSON_HYPHAE -> SmeltResult(Material.CHARCOAL, 0.0)
+            Material.CRIMSON_STEM -> SmeltResult(Material.CHARCOAL, 0.0)
+            Material.STRIPPED_CRIMSON_HYPHAE -> SmeltResult(Material.CHARCOAL, 0.0)
+            Material.STRIPPED_CRIMSON_STEM -> SmeltResult(Material.CHARCOAL, 0.0)
             else -> null
         }
     }
