97544138f2a83c937f706a2da80d044afa05aab6
Goh Wei Wen <goweiwen@gmail.com>
2025-03-13 00:00:33 +0800

    Allow brushing pets

--- BlanktopiaTweaks/src/main/kotlin/me/weiwen/blanktopia/tweaks/modules/Petting.kt
+++ BlanktopiaTweaks/src/main/kotlin/me/weiwen/blanktopia/tweaks/modules/Petting.kt
@@ -13,6 +13,7 @@ import org.bukkit.event.Listener
 import org.bukkit.event.block.Action
 import org.bukkit.event.player.PlayerInteractEntityEvent
 import org.bukkit.event.player.PlayerInteractEvent
+import org.bukkit.inventory.EquipmentSlot
 import org.bukkit.plugin.java.JavaPlugin
 import org.bukkit.util.Vector
 import kotlin.math.min
@@ -37,20 +38,46 @@ class Petting(private val plugin: JavaPlugin) :
 
     @EventHandler(priority = EventPriority.HIGHEST, ignoreCancelled = true)
     fun onPlayerInteractEntity(event: PlayerInteractEntityEvent) {
+        if (event.hand != EquipmentSlot.HAND) return
+
         val entity = event.rightClicked
 
         if (entity.type !in pettableEntities) return
 
-        if (!event.player.isSneaking) return
+        var isBrush = false
+        if (event.player.inventory.itemInMainHand.type == Material.BRUSH && entity.type != EntityType.ARMADILLO) {
+            isBrush = true
+        } else if (!event.player.isSneaking) return
 
         if (entity is Damageable && entity is Attributable) {
             val maxHealth = entity.getAttribute(Attribute.MAX_HEALTH)?.value ?: return
             entity.health = min(entity.health + 1, maxHealth)
         }
 
-        entity.world.spawnParticle(Particle.HEART, entity.location.add(0.0, entity.height + 0.2, 0.0), 1)
+        if (isBrush) {
+            entity.world.spawnParticle(
+                Particle.HAPPY_VILLAGER,
+                entity.location.add(0.0, entity.height + 0.2, 0.0),
+                10,
+                0.5,
+                0.5,
+                0.5,
+            )
+            event.player.world.playSound(event.player.eyeLocation, Sound.ITEM_BRUSH_BRUSHING_SAND, 1.0f, 1.0f)
+            event.player.swingMainHand()
+        } else {
+            entity.world.spawnParticle(
+                Particle.HEART,
+                entity.location.add(0.0, entity.height + 0.2, 0.0),
+                1,
+                0.5,
+                0.5,
+                0.5,
+            )
+            event.player.world.playSound(event.player.eyeLocation, Sound.ENTITY_PLAYER_ATTACK_WEAK, 0.5f, 1.0f)
+            event.player.swingMainHand()
+        }
 
-        event.player.world.playSound(event.player.eyeLocation, Sound.ENTITY_PLAYER_ATTACK_WEAK, 0.5f, 1.0f)
 
         event.isCancelled = true
     }
